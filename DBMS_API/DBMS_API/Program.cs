05a2d6cf430b6f3b459737a8f59ec481145c2189
Scorpion259 <63725288+Scorpion259@users.noreply.github.com>
2022-06-07 17:52:59 +0300

    Add files via upload

--- DBMS_API/DBMS_API/Program.cs
+++ DBMS_API/DBMS_API/Program.cs
@@ -1,6 +1,8 @@
 using DBMS_API.DataAccess;
 using DBMS_API.Domain.Models.MarketingDepartment;
+using DBMS_API.Domain.Models.Provider;
 using DBMS_API.Properties.Common;
+using DBMS_API.Repositories;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -17,6 +19,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton(typeof(MarketingDepartmentContext))
             .AddSingleton(typeof(IRepository<MarketingDepartmentContext>), typeof(MarketingRepository))
             .AddDbContextPool<MarketingDepartmentContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("MarketingConnection")));
+
+builder.Services.AddSingleton(typeof(ServiceDepartmentContext))
+           .AddSingleton(typeof(IRepository<ServiceDepartmentContext>), typeof(ServiceRepository))
+           .AddDbContextPool<ServiceDepartmentContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("ServiceConnection")));
+
+builder.Services.AddSingleton(typeof(ProviderContext))
+           .AddSingleton(typeof(IMongoRepository), typeof(MongoRepository));
+
+builder.Services.Configure<ProviderContext>(
+    builder.Configuration.GetSection("MongoDbSettings"));
 
 builder.Services
 .AddAutoMapper(typeof(BaseAppMappingProfile))
@@ -58,14 +70,25 @@ app.UseEndpoints(endpoints =>
 app.Run();
 
 
-/*
-var context = new MarketingDepartmentContext();
+
+/*var context = new MarketingDepartmentContext();
 var repository = new Repository(context);
 var list = await repository.GetAllAsync<Car>();
-
-
 foreach (var item in list)
 {
     Console.WriteLine(item.Price);
-}
-*/
+}*/
+
+
+/*var repository = new MongoRepository();
+var list = await repository.GetAllAsync();
+
+Console.WriteLine("2222222222222222222");
+foreach (var item in list)
+{
+    if(item.CarsObj != null)
+    {
+        Console.WriteLine(item.CarsObj[0].Body);
+    }
+}*/
+
